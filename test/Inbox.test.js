eeb40363f287d3886fd9b159f184bbfa78b9211d
danale <danale@danales-MacBook-Pro.local>
2018-04-06 09:18:22 -0400

    Implemented web3 version fix

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -18,10 +21,16 @@ beforeEach(async () => {
       arguments: ['hi there']
     })
     .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
 });
 
 describe('inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'hi there');
   });
 });
